763bcc400ea89d87dded9588a8896ce1a225cfe6
Jennifer <jennifermdevine88@gmail.com>
2023-08-24 21:08:22 -0700

    followed along in class all sessions

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // DEPENDENCIES
 const express = require('express')
-const app = express()
+const app = express();
+const { Sequelize } = require('sequelize');
+
+const bandsController = require('./controllers/bands_controller');
+const eventsController = require('./controllers/events_controller');
+const stagesController = require('./controllers/stages_controller');
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/bands', bandsController);
+app.use('/events', eventsController);
+app.use('/stages', stagesController);
 
 // ROOT
 app.get('/', (req, res) => {
